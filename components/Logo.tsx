ad49737a338e8d729f3ffd6faecd1ef19ac043f5
alvinle2901 <bach938@gmail.com>
2024-10-23 12:57:16 +0200

    Update logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,5 +1,6 @@
-import Link from "next/link";
-import React from "react";
+import Link from 'next/link';
+import React from 'react';
+import Image from 'next/image'
 
 interface Props {
   title?: string;
@@ -8,10 +9,13 @@ interface Props {
 
 const Logo = ({ title, className }: Props) => {
   return (
-    <Link href={"/"}>
-      <h1 className={`text-3xl font-extrabold uppercase ${className}`}>
+    <Link href={'/'}>
+      {/* <h1 className={`text-3xl font-extrabold uppercase ${className}`}>
         {title || "Bloggers"}
-      </h1>
+      </h1> */}
+      {/* <img src={'../public/logo.png'} className="" /> */}
+
+      <Image src="/logo1.png" width={120} height={60} alt="logo" />
     </Link>
   );
 };
